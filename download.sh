8c28a32637d5857f1088741120aaa6822d5fbb74
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-26 10:31:42 +0900

    fallback to jFrog

--- download.sh
+++ download.sh
@@ -14,7 +14,18 @@ PATCH=$(echo "$VERSION" | cut -d. -f3)
 
 cd "$CURRENT/download/boost-mirror-$VERSION"
 
-curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.tar.gz"
-curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.zip"
-curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.tar.bz2"
-curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.7z" || rm -f "boost_${MAJOR}_${MINOR}_${PATCH}.7z"
+curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.tar.gz" || \
+    curl -f --retry 3 -LO "https://boostorg.jfrog.io/artifactory/main/release/${MAJOR}.${MINOR}.${PATCH}/source/boost_${MAJOR}_${MINOR}_${PATCH}.tar.gz" || \
+    rm -f "boost_${MAJOR}_${MINOR}_${PATCH}.tar.gz"
+
+curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.zip" || \
+    curl -f --retry 3 -LO "https://boostorg.jfrog.io/artifactory/main/release/${MAJOR}.${MINOR}.${PATCH}/source/boost_${MAJOR}_${MINOR}_${PATCH}.zip" || \
+    rm -f "boost_${MAJOR}_${MINOR}_${PATCH}.zip"
+
+curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.tar.bz2" || \
+    curl -f --retry 3 -LO "https://boostorg.jfrog.io/artifactory/main/release/${MAJOR}.${MINOR}.${PATCH}/source/boost_${MAJOR}_${MINOR}_${PATCH}.tar.bz2" || \
+    rm -f "boost_${MAJOR}_${MINOR}_${PATCH}.tar.bz2"
+
+curl -f --retry 3 -LO "https://sourceforge.net/projects/boost/files/boost/${MAJOR}.${MINOR}.${PATCH}/boost_${MAJOR}_${MINOR}_${PATCH}.7z" || \
+    curl -f --retry 3 -LO "https://boostorg.jfrog.io/artifactory/main/release/${MAJOR}.${MINOR}.${PATCH}/source/boost_${MAJOR}_${MINOR}_${PATCH}.7z" || \
+    rm -f "boost_${MAJOR}_${MINOR}_${PATCH}.7z"
